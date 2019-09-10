f39c6ef742d6f4c649b78acd5c0142dec2395c51
Miguelfacundo100 <51345969+Miguelfacundo100@users.noreply.github.com>
2019-09-10 17:31:04 -0500

    Create check-login.php

---
+++ check-login.php
@@ -0,0 +1,73 @@
+<?php
+session_start();
+?>
+
+<!doctype html>
+<html lang="en">
+	<head>
+		<title>Mi perfil</title>
+		<!-- Required meta tags -->
+		
+
+		<meta charset="UFT-8">
+            <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1">
+            <link rel="stylesheet" href="estilo.css">
+	</head>
+	<body>
+		<div class="container">
+		
+			<?php
+			// Coneccion
+			include 'conn.php';	
+			
+			
+			$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+			//verifica la coneccion
+			if (!$conn) {
+				die("Connection failed: " . mysqli_connect_error());
+			}
+			
+			// data sent from form login.html 
+			$email = $_POST['email']; 
+			$password = $_POST['password'];
+			
+			// Query sent to database
+			$result = mysqli_query($conn, "SELECT Email, Password, Name FROM users WHERE Email = '$email'");
+			
+			// Variable $row hold the result of the query
+			$row = mysqli_fetch_assoc($result);
+			
+			// Variable $hash hold the password hash on database
+			$hash = $row['Password'];
+			
+			/* 
+			password_Verify() function verify if the password entered by the user
+			match the password hash on the database. If everything is OK the session
+			is created for one minute. Change 1 on $_SESSION[start] to 5 for a 5 minutes session.
+			*/
+			if (password_verify($_POST['password'], $hash)) {	
+				
+				$_SESSION['loggedin'] = true;
+				$_SESSION['name'] = $row['Name'];
+				$_SESSION['start'] = time();
+				$_SESSION['expire'] = $_SESSION['start'] + (1 * 60) ;						
+				
+				echo "<div class='alert alert-success mt-4' role='alert'><strong>BIENVENIDO </strong> $row[Name]			
+				<p><a href='edit-profile.php'>Editar perfil</a></p>	
+				<p><a href='logout.php'>Cerrar Cesion</a></p></div>";	
+			
+			} else {
+				echo "<div class='alert alert-danger mt-4' role='alert'>Email or Password are incorrects!
+				<p><a href='login.html'><strong>Please try again!</strong></a></p></div>";			
+			}	
+			?>
+		</div>
+		<!-- Optional JavaScript -->
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+
+	</body>
+</html>
