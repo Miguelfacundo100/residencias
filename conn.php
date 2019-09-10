3662a1d6102b720ca8ea6af54231ecbd64d20084
Miguelfacundo100 <51345969+Miguelfacundo100@users.noreply.github.com>
2019-09-10 17:29:53 -0500

    Create conn.php

---
+++ conn.php
@@ -0,0 +1,7 @@
+<?php
+// Connection variables
+$dbhost	= "localhost";	   // localhost or IP
+$dbuser	= "root";		  // database username
+$dbpass	= "miguel10";		     // database password
+$dbname	= "phplogin";    // database name
+?>
